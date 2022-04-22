d07193e948ae3dc1e1f938ab472e8f6f1bffe74f
Allison Demkovich <demkovich.allison@gmail.com>
2022-04-22 11:51:12 -0500

    initial commit, set up react

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='wrapper'>
+        {/* navbar */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
